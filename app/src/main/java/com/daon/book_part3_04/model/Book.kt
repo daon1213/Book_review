6a7925fc3986fdcd52c56da9ea8a5f91a5cd86ee
daon1213 <diaeiyo1213@naver.com>
2022-04-11 08:42:11 +0900

    Book commit!

---
+++ app/src/main/java/com/daon/book_part3_04/model/Book.kt
@@ -0,0 +1,13 @@
+package com.daon.book_part3_04.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Book(
+    @SerializedName("itemId") val id: Long,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("converSmallUrl") val converSmallUrl: String
+): Parcelable
