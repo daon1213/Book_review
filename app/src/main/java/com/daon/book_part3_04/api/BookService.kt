c7aef3700b7fd8c47b7e4e16f9acc5f7a245093a
daon1213 <diaeiyo1213@naver.com>
2022-04-11 08:43:23 +0900

    Book Service commit!

---
+++ app/src/main/java/com/daon/book_part3_04/api/BookService.kt
@@ -0,0 +1,20 @@
+package com.daon.book_part3_04.api
+
+import com.daon.book_part3_04.model.BestSellerDto
+import com.daon.book_part3_04.model.SearchBookDto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BookService {
+    @GET("/api/search.api?output=json")
+    fun getBooksByName(
+        @Query("key") apiKey: String,
+        @Query("query") keyword: String
+    ): Call<SearchBookDto>
+
+    @GET("/api/bestSeller.api?output=json&categoryId=100")
+    fun getBestSellerBooks(
+        @Query("key") apiKey: String
+    ): Call<BestSellerDto>
+}
